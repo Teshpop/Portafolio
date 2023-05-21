67230e921e3c5fe228c78646bbd958687fd77e63
Teshpop <tello.3810@outlook.com>
2023-05-21 01:51:40 -0600

    Creacion de portafolio

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { About, Header, Hero, Experience } from "./components";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div>
+          <Header />
+        </div>
+        <Hero />
+        <About />
+        <Experience />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import About from "./About";
+import Experience from "./Experience";
+import Header from "./Header";
+import Hero from "./Hero";
+
+export { About, Experience, Header, Hero };
